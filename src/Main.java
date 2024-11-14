7a09c11622ae31433b79c17b92f1dbc433be76ab
AngelicaSjodin <angelic.sjodin@gmail.com>
2024-11-14 18:27:58 +0100

    grid?

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        String line =
+                "o|o|o\n"+
+                "-+-+-\n"+
+                "o|o|o\n"+
+                "-+-+-\n"+
+                "o|o|o";
+
+        System.out.println(line);
+    }
+
+}
